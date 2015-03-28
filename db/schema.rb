8fe0069213afd51b7703349bc2a716fa679976c9
Volmer <git@radicaos.com>
2015-03-28 12:19:45 -0400

    Create Build model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150328151651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "builds", force: :cascade do |t|
+    t.integer  "pull_request",                     null: false
+    t.string   "repo",                             null: false
+    t.string   "user",                             null: false
+    t.string   "sha",                              null: false
+    t.string   "status",       default: "pending", null: false
+    t.text     "payload",                          null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
 
 end
