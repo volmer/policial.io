dcfd16008d4ea72d1b9bf2f1d74f2a8861dad452
Arthur Neves <arthurnn@gmail.com>
2015-04-08 18:07:19 -0400

    need access to hooks on login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
+           scope: 'user,repo,write:repo_hook'
 end
