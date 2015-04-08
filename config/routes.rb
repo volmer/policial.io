631f014855fbdd2a6a77565a24e489ffa48a3ed6
Arthur Neves <arthurnn@gmail.com>
2015-04-08 18:25:14 -0400

    Add repositories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :builds, only: [:index, :show, :create]
+  resources :repositories, only: [:index, :create]
 
   get '/auth/github/callback', to: 'auth#github'
   get '/logout', to: 'auth#logout'
