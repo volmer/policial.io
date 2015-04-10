32293bac93495d151acc9b419c5dc52c7e583a65
Arthur Neves <arthurnn@gmail.com>
2015-04-09 21:25:36 -0400

    Add render_404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,19 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :alert, :success
 
+  rescue_from(ActiveRecord::RecordNotFound) { render_404 }
+
   private
+
+  def render_404
+    respond_to do |format|
+      format.html do
+        render file: "#{Rails.root}/public/404.html",
+               layout: false, status: 404
+      end
+      format.all { render nothing: true, status: 404 }
+    end
+  end
 
   def current_user=(user)
     session[:current_user] = user.as_json
