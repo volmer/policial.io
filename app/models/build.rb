11fb0576d19273d61fb5d17a742638b178d7f89c
Volmer <git@radicaos.com>
2015-04-19 23:34:09 -0400

    Use Repository#github_client to run investigations

--- app/models/build.rb
+++ app/models/build.rb
@@ -16,14 +16,14 @@ class Build < ActiveRecord::Base
   # Returns a new Build with the pull request information.
   def self.from_webhook(payload)
     json = JSON.parse(payload)
-    pull_request = Policial::PullRequestEvent.new(json).pull_request
+    pull_request = Policial::PullRequestEvent.new(json).pull_request_attributes
     return if pull_request.nil?
 
     new(
-      pull_request: pull_request.number,
-      repo: pull_request.repo,
-      user: pull_request.user,
-      sha: pull_request.head_commit.sha,
+      pull_request: pull_request[:number],
+      repo: pull_request[:repo],
+      user: pull_request[:user],
+      sha: pull_request[:head_sha],
       payload: payload
     )
   end
